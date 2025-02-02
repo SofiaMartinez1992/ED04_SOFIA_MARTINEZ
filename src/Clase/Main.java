273b53e357511543c43e22c77c57f89e1d0d3522
SofiaMartinez1992 <sofi_m_g@hotmail.com>
2025-02-02 13:10:34 +0100

    Primer commit

---
+++ src/Clase/Main.java
@@ -0,0 +1,31 @@
+package Clase;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(2300.0f);
+    }
+
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.println("Fallo al retirar");
+        }
+
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.println("Fallo al ingresar");
+        }
+    }
+}
